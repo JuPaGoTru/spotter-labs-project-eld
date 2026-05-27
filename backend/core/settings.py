3ef26a3d71971b4b193c5b8f4d64fd2ab848e10b
Juan Pablo Gonzalez <118215306+JuPaGoTru@users.noreply.github.com>
2026-05-26 20:10:50 -0500

    Update CORS settings based on DEBUG environment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -82,7 +82,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOWED_ORIGINS = os.environ.get(
+    'CORS_ALLOWED_ORIGINS', ''
+).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 # ORS API Key
